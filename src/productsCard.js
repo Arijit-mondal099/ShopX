6d4a669c2cbed32ec4cba33c26acdff4c5d9e762
Arijit Mondal <arijitm717@gmail.com>
2024-12-27 21:05:25 +0530

    Update productsCard.js
    update

--- src/productsCard.js
+++ src/productsCard.js
@@ -1,4 +1,4 @@
-import { addToCard } from "./addToCard";
+import { addToCard } from "./addToCard.js";
 import { quantityToggle } from "./quantityToggle.js";
 
 const itemCardsContainer = document.querySelector(".itemCardsContainer");
