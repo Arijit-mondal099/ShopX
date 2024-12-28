ae21279b0faafa7e444a5e5a6fc3fafd304989a8
Arijit Mondal <arijitm717@gmail.com>
2024-12-28 19:05:23 +0530

    Update quantityToggle.js
    update

--- src/quantityToggle.js
+++ src/quantityToggle.js
@@ -2,14 +2,14 @@ export function quantityToggle(event, itemId, itemStock) {
   // fetch current card
   const currProductCard = document.querySelector(`#card${itemId}`);
 
-  // fetch current card quantity(display)
+  // fetch current card quantity(display) and fetch current product quantity
   const productQuantity = currProductCard.querySelector(".display");
   let quantity = parseInt(productQuantity.getAttribute("data-quantity")) || 1;
 
-  // check stock is avilable or not
+  // check stock is available or not
   if (itemStock === "comming soon") {
     quantity = 0;
-  } else {
+  } else { // available
     if (event.target.className === "addOne") {
       if (quantity < itemStock) quantity++;
     } else if (event.target.className === "subOne") {
