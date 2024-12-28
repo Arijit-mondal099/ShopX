f6a02ea0570a85717c736bcfb02fc5764e279800
Arijit Mondal <arijitm717@gmail.com>
2024-12-28 18:48:06 +0530

    Update showAddToCardProducts.js
    update

--- src/showAddToCardProducts.js
+++ src/showAddToCardProducts.js
@@ -1,11 +1,9 @@
 import { removeProduct } from "./removeProduct.js";
-
-// some problem occurred (61, 62) line so we put price & quantity manually 
-// import products from "../productsApi/products.json";
-
 import { incrimentDecrement } from "./incrimentDecrement.js";
 import { showAllPriceSummary } from "./showAllPriceSummary.js";
 
+
+// fetch card template and card container
 const cardProductsContainer = document.querySelector(".cards");
 const productsCardContainer = document.querySelector(".productsCardContainer");
 
@@ -13,59 +11,36 @@ export function showAddToCardProducts(cardProducts) {
   cardProductsContainer.innerHTML = "";
   if (cardProducts.length > 0) {
     cardProducts.forEach((product) => {
-      const {
-        category,
-        currProductImg,
-        itemId,
-        productName,
-        quantity,
-        totalPrise,
-      } = product;
+      // destructer data from current product
+      const {category, currMoney, currProductImg, currStock, itemId, productName, quantity, totalPrise} = product;
       
       // clone card from card template
-      const productCardClone = document.importNode(
-        productsCardContainer.content,
-        true
-      );
+      const productCardClone = document.importNode(productsCardContainer.content, true);
 
-      // ../public/products/mobile.png --> /public/products/mobile.png
+      // /public/products/mobile.png
       const img = currProductImg.split(".com")[1];
 
       // add unic id to all
-      productCardClone
-        .querySelector("#cardVal")
-        .setAttribute("id", `card${itemId}`);
+      productCardClone.querySelector("#cardVal").setAttribute("id", `card${itemId}`);
 
       // show all data
       productCardClone.querySelector(".tag").textContent = category;
       productCardClone.querySelector(".productImg").src = img;
       productCardClone.querySelector(".productName").textContent = productName;
-      productCardClone.querySelector(
-        ".currMoney"
-      ).textContent = `₹${totalPrise.toFixed(2)}`;
+      productCardClone.querySelector(".currMoney").textContent = `₹${totalPrise.toFixed(2)}`;
       productCardClone.querySelector(".display").textContent = quantity;
 
       // event to remove product from card
-      productCardClone
-        .querySelector(".remove")
+      productCardClone.querySelector(".remove")
         .addEventListener("click", (event) => {
           event.preventDefault();
           removeProduct(itemId);
         });
 
-      // find current product stock
-      // const currProduct = products.find((ele) => ele.id === itemId);
-      // const stock = currProduct.stock;
-      // const price = currProduct.price1;
-      
-      const stock = 100;
-      const price = 100;
-
       // event to add and sub products
-      productCardClone
-        .querySelector(".box")
+      productCardClone.querySelector(".box")
         .addEventListener("click", (event) => {
-          incrimentDecrement(event, itemId, stock, price);
+          incrimentDecrement(event, itemId, currStock, currMoney);
         });
 
       // show all price
@@ -74,5 +49,10 @@ export function showAddToCardProducts(cardProducts) {
       // append card into container
       cardProductsContainer.appendChild(productCardClone);
     });
+
+
+
+
+    
   }
 }
