058c9205b61f19d28faa55d94898f808f796d10a
Arijit Mondal <arijitm717@gmail.com>
2024-12-28 19:18:07 +0530

    Update showAllPriceSummary.js
    update

--- src/showAllPriceSummary.js
+++ src/showAllPriceSummary.js
@@ -1,6 +1,7 @@
 import { getCardProductsDB } from "./getCardProducts.js";
 
 export function showAllPriceSummary() {
+  // fetch all summary containers
   const total = document.querySelector(".total");
   const tax = document.querySelector(".taxval");
   const final = document.querySelector(".final");
@@ -9,15 +10,15 @@ export function showAllPriceSummary() {
   // get all products from db and calculate all prise
   const cardProducts = getCardProductsDB();
 
-  if (cardProducts.length > 0) {
-    const allPrise = cardProducts.map((ele) => ele.totalPrise);
-    const totalPrice = allPrise.reduce((acc, curr) => acc + curr, 0);
-
+  if (cardProducts.length > 0) { // if product is exist
+    const allPrise = cardProducts.map((ele) => ele.totalPrise); // fetch all products total prise
+    const totalPrice = allPrise.reduce((acc, curr) => acc + curr, 0); // add all products total prise
+    // show all products summary
     totalItem.textContent = cardProducts.length;
     total.textContent = `₹${totalPrice.toFixed(2)}`;
     tax.textContent = `₹${40}`;
     final.textContent = `₹${(totalPrice + 40).toFixed(2)}`;
-  } else {
+  } else { // if product innot exit
     totalItem.textContent = cardProducts.length;
     total.textContent = `₹${0}`;
     tax.textContent = `₹${0}`;
